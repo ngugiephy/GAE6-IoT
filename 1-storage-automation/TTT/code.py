11cc0ef69905a40b231cc34f7480de5701a79355
ngugiephy <ngugiephy@gmail.com>
2022-11-07 14:30:15 +0300

    updates

---
+++ 1-storage-automation/TTT/code.py
@@ -0,0 +1,56 @@
+import network
+import time
+from machine import Pin
+from umqttsimple import MQTTClient
+
+wlan=network.WLAN(network.STA_IF)
+wlan.active(True)
+
+wlan.connect("Gearbox Staff","Staff@Gearbox")
+time.sleep(5)
+print(wlan.isconnected())
+
+lock=Pin(0,Pin.OUT)
+pinIn = Pin(2, Pin.IN,Pin.PULL_UP)
+status=Pin(1,Pin.OUT)
+
+mqtt_server='192.168.0.212'
+client_id='Pico'
+topic_sub='locker'
+
+def message(topic,msg):
+    print("New message on topic {}".format(topic.decode('utf-8')))
+    msg=msg.decode('utf-8')
+    print(msg)
+    if msg=="open":
+        lock.on()    
+        time.sleep(1)
+        lock.off()
+       
+def mqtt_connect():
+        client=MQTTClient(client_id,mqtt_server)
+        client.set_callback(message)
+        client.connect()
+        print('Connected to %s MQTT Broker'%(mqtt_server))
+        return client
+   
+def recconect():
+        print('Failed to connect to MQTT Broker. Reconnecting...')
+        time.sleep(5)
+        machine.reset()
+
+
+try:
+    client=mqtt_connect()
+except OSError as e:
+    recconect()
+while True:
+    client.subscribe(topic_sub)
+    status.off()
+    time.sleep(5)
+    status.on()
+    if pinIn.value():
+        client.publish("status","Locker opened")
+    else:
+        client.publish("status","Locker is closed")
+    
---
+++ 1-storage-automation/readme.md
@@ -0,0 +1,8 @@
+# GAE Cabinets Automation
+
+Android Studio application for opening cabinets.
+
+* Firebase
+* Raspberry PI
+* MQTT Protocol
+* DialogBox
