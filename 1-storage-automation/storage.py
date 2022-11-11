44256512364d9dd5d6ea1d54142ebe5b8523f52f
BNdungu <bryannganga909@gmail.com>
2022-11-11 14:10:23 +0300

    storage.py

---
+++ 1-storage-automation/storage.py
@@ -0,0 +1,58 @@
+import network
+import time
+from machine import Pin
+from umqttsimple import MQTTClient
+
+wlan=network.WLAN(network.STA_IF)
+wlan.active(True)
+
+
+wlan.connect("Raspberry(Cloud & IOT","abraham123")
+time.sleep(5)
+print(wlan.isconnected())
+
+locker=Pin(0,Pin.OUT)
+status=Pin(1,Pin.OUT)
+
+
+mqtt_server='192.168.1.4'
+client_id='Pico_locker'
+topic_sub='bench/locker'
+
+def message(topic,msg):
+    print("New message on topic {}".format(topic.decode('utf-8')))
+    msg=msg.decode('utf-8')
+    print(msg)
+    if msg=="on":
+        locker.on()   
+        time.sleep(1)
+        locker.off() 
+   
+        
+       
+def mqtt_connect():
+        client=MQTTClient(client_id,mqtt_server)
+        client.set_callback(message)
+        client.connect()
+        print('Connected to %s MQTT Broker'%(mqtt_server))
+        return client
+   
+def recconect():
+        print('Failed to connect to MQTT Broker. Reconnecting...')
+        time.sleep(5)
+        machine.reset()
+
+
+try:
+    client=mqtt_connect()
+except OSError as e:
+    recconect()
+while True:
+    client.subscribe(topic_sub)
+    status.off() 
+    time.sleep(5)
+    status.on()
+    
+    
+
+
